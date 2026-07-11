1d0a88944d7a391abb61e105206fb57db0965639
shiv kumar <kumarsahashiv@gmail.com>
2026-07-11 16:09:42 +0530

    update index.html and script.js

--- script.js
+++ script.js
@@ -20,104 +20,29 @@ function menuToggle() {
 const projects =
     [
         {
-            projectImageURL: "./assets/rest-country.png",
-            projectName: "REST Countries API with color theme switcher",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Rest-Countries-Api-With-Color-Theme-Switcher-Master/",
-            codeURL: "https://github.com/Rajiv-0920/Rest-Countries-Api-With-Color-Theme-Switcher-Master.git",
-            challengeBy: "Frontend Mentor",
-            challengeByURL: "https://www.frontendmentor.io/"
-        },
+            projectImageURL: "/assets/liveobject.png",
+            projectName: "live obejct detection",
+            projectTags: ["python","ai"],
+            projectURL: "https://github.com/Shivkumarsaha/live-object-detection.git",
+            codeURL: "https://github.com/Shivkumarsaha/live-object-detection/blob/main/camtest.py",
+            
+        }
+        ,
         {
-            projectImageURL: "./assets/manage.png",
-            projectName: "Manage Landing Page",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Manage-landing-page/",
-            codeURL: "https://github.com/Rajiv-0920/Manage-landing-page.git",
-            challengeBy: "Frontend Mentor",
-            challengeByURL: "https://www.frontendmentor.io/"
-        },
-        {
-            projectImageURL: "./assets/blogr.png",
-            projectName: "Blogr Landing Page",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://Rajiv-0920.github.io/Blogr-landing-page",
-            codeURL: "https://github.com/Rajiv-0920/Blogr-landing-page.git",
-            challengeBy: "Frontend Mentor",
-            challengeByURL: "https://www.frontendmentor.io/"
-        },
-        {
-            projectImageURL: "./assets/rock-paper-scissor.png",
-            projectName: "Rock Paper Scissor Game",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Rock-Paper-Scissors/",
-            codeURL: "https://github.com/Rajiv-0920/Rock-Paper-Scissors.git",
-            challengeBy: "Frontend Mentor",
-            challengeByURL: "https://www.frontendmentor.io/"
-        },
-        {
-            projectImageURL: "./assets/easybank.png",
-            projectName: "Easy Bank Landing Page",
-            projectTags: ["HTML", "CSS"],
-            projectURL: "https://rajiv-0920.github.io/Easybank-landing-page/",
-            codeURL: "https://github.com/Rajiv-0920/Easybank-landing-page.git",
-            challengeBy: "Frontend Mentor",
-            challengeByURL: "https://www.frontendmentor.io/"
-        },
-        {
-            projectImageURL: "./assets/NFT.png",
-            projectName: "NFT Landing Page",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/NFT-Landing-Page/",
-            codeURL: "https://github.com/Rajiv-0920/NFT-Landing-Page.git",
-            challengeBy: "Lorem picsum",
-            challengeByURL: "#"
-        },
-        {
-            projectImageURL: "./assets/translator.png",
-            projectName: "Language Translator",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Language-Translator/",
-            codeURL: "https://github.com/Rajiv-0920/Language-Translator.git",
-            challengeBy: "Lorem picsum",
-            challengeByURL: "#"
-        },
-        {
-            projectImageURL: "./assets/qr-code.png",
-            projectName: "QR Code Generator",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/QR-Code-Generator/",
-            codeURL: "https://github.com/Rajiv-0920/QR-Code-Generator.git",
-            challengeBy: "Lorem picsum",
-            challengeByURL: "#"
-        },
-        {
-            projectImageURL: "./assets/wordcounter.png",
-            projectName: "Word & Character Counter",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Word-Count/",
-            codeURL: "https://github.com/Rajiv-0920/Word-Count.git",
-            challengeBy: "Lorem picsum",
-            challengeByURL: "#"
-        },
-        {
-            projectImageURL: "./assets/tic-tac-toe.png",
-            projectName: "Tic Tac Toe Game",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Tic-Tac-Toe/",
-            codeURL: "https://github.com/Rajiv-0920/Tic-Tac-Toe.git",
-            challengeBy: "Lorem picsum",
-            challengeByURL: "#"
-        },
-        {
-            projectImageURL: "./assets/github.png",
-            projectName: "Github User Search",
-            projectTags: ["HTML", "CSS", "JS"],
-            projectURL: "https://rajiv-0920.github.io/Github-user-search/",
-            codeURL: "https://github.com/Rajiv-0920/Github-user-search.git",
-            challengeBy: "Frontend Mentor",
-            challengeByURL: "https://www.frontendmentor.io/"
-        },
+            projectImageURL: "/assets/stdgradesystem.png",
+            projectName: "Student Grade Management System",
+            projectTags: ["JAVA","Swing","MySQL"],
+            projectURL: "https://github.com/Shivkumarsaha/student-grade-management-system.git",
+            codeURL: "https://github.com/Shivkumarsaha/student-grade-management-system/blob/main/src/ui/StudentGradeManagementSystem.java",
+         }
+         ,
+         {
+          projectImageURL: "/assets/lock.jpeg",
+            projectName: "RFID And Keypad Based Door Lock System",
+            projectTags: ["ArduinoUno","RFID","Sensor"],
+            projectURL: "https://github.com/Shivkumarsaha/RFID-and-Kepad-Based-Door-Lock-System-Using-Arduino/tree/main/RFID-Keypad-Smart-Door-Lock-IoT",
+            codeURL: "https://github.com/Shivkumarsaha/RFID-and-Kepad-Based-Door-Lock-System-Using-Arduino/blob/main/RFID-Keypad-Smart-Door-Lock-IoT/FINAL%20CODE%20OF%20IOT.txt",
+         }
     ]
 
 
